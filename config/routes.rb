20772aae345e0a9e9b58beba276363161ae4ab91
Allen Anderson <dande313@byu.net>
2017-10-12 12:08:33 -0400

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-	
 
-	
+
+
   resources :articles
-  resources :departments
-  resources :ships
+  resources :departments, only: [:show, :index]
   devise_for :users
   root "home#index"
 end
