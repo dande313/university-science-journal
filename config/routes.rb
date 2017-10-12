d97060b172bd6ee0af06f0486f87b71d79e86c2b
Allen Anderson <dande313@byu.net>
2017-10-12 13:22:57 -0400

    Added ability for users to submit articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-
-
   resources :articles
   resources :departments, only: [:show, :index]
   devise_for :users
+
   root "home#index"
+  get '/articles/:id/destroy', to: 'articles#destroy'
 end
